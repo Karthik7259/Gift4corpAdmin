cbefa688e26ae327f87560b966b974b111051dd8
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-12-12 12:15:22 +0530

    home page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
             <NavLink to='/' className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l ' >
                  <p className='text-xl'>📊</p>
-                 <p className='hidden md:block'>Dashboard</p>
+                 <p className='hidden md:block'>Home</p>
             </NavLink>
             <NavLink to='/add' className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l ' >
                  <img className='w-5 h-5' src={assets.add_icon} alt="" />
